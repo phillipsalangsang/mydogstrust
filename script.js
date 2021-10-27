132d0dff0a6a07a78b7a215d655bf166a65435cd
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-27 14:44:09 +0100

    changes to the css and the js

--- script.js
+++ script.js
@@ -15,20 +15,30 @@ dogs.push({name:"Alfie",Breed:"Shiba Inu",age:3,picture:""})
 // Add an age property for every dog
 // add an 'Adopt Now' button on each dog
 
-for(let i=0;i<dogs.length;i++){
+for (let i=0; i<dogs.length; i++){
 
     let card = document.createElement("div")
     document.body.appendChild(card)
-    card.classList.add("card")
-    let dogName=document.createElement("h1")
-    card.appendChild(dogName)
-
-    let dogName=document.createElement("h2")
-    card.appendChild(dogName)
     
-    dogName.innerHTML=dogs[i].name
-
-    dogName.innerHTML=dogs[i].name
-
+    let dogName = document.createElement("h1")
+    card.appendChild(dogName)
+    document.body.style.backgroundColor = "yellow" //Changes background colour
+    card.style.backgroundColor = "blue" //Changes colour of the card
+    
+    let dogBreed = document.createElement("h2")
+    card.appendChild(dogBreed)
+    
+    let dogPicture = document.createElement("img")
+    card.appendChild(dogPicture)
+    dogPicture.src = dogs[i].Picture
+    
+    let dogAge = document.createElement("p")
+    card.appendChild(dogAge)
+    card.style.padding = 300 + "px"
+    card.style.margin = 400 + "px"
+    dogName.innerHTML = dogs[i].Name
+    dogBreed.innerHTML = dogs[i].Breed
+    dogPicture.innerHTML = dogs[i].Picture
+    dogAge.innerHTML = dogs[i].Age
 }
 
